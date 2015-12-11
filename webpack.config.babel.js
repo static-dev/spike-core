1b4c0fea3590b7fc02ec635c9a95a359a4cfe56e
kylemac <kyle@carrotcreative.com>
2015-12-11 15:09:46 -0500

    prune deps and currently unused configuration

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,6 +1,4 @@
 import path from 'path'
-import autoprefixer from 'autoprefixer'
-import precss from 'precss'
 import JadePlugin from './plugins/jade_plugin'
 
 module.exports = {
@@ -11,13 +9,8 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.css$/, exclude: /node_modules/, loader: 'style-loader!css-loader!postcss-loader' },
-      // { text: /\.js$/, exclude: /node_modules, plugins/, loader: 'babel-loader?presets[]=es2015' },
       { test: /\.jade$/, exclude: /node_modules/, loader: 'jade-loader' }
     ]
-  },
-  postcss: function () {
-    return [autoprefixer, precss]
   },
   plugins: [
     new JadePlugin()

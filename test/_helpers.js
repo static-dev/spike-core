1930c443697805e07c4c5e10c53586db53eb940b
Declan de Wet <declandewet@me.com>
2015-12-25 17:37:23 +0200

    document + refactor test helpers

--- test/_helpers.js
+++ test/_helpers.js
@@ -4,6 +4,13 @@ import Roots from '..'
 
 const fixtures = path.join(__dirname, 'fixtures')
 
+/**
+ * compiles a fixture into it's `public/` directory
+ * @param  {Object} t - ava test helper for setting t.context props
+ * @param  {String} name - the name of the fixture to compile
+ * @return {Promise} - a promise for the compiled fixture and the path to
+ *                     it's `public/` directory
+ */
 async function compileFixture (t, name) {
   let testPath = path.join(fixtures, name)
   let project = new Roots({ root: testPath })
@@ -12,6 +19,15 @@ async function compileFixture (t, name) {
   return { res, publicPath }
 }
 
+/**
+ * converts a function that accepts a node-style callback
+ * into a function that returns a promise
+ * @param  {Function} original - the function to convert
+ * @return {Function} the promisified function
+ * @example
+ * let readFile = promisify(fs.readFile)
+ * readFile(file).then(...)
+ */
 function promisify (original) {
   return (...args) => {
     return new Promise((resolve, reject) => {
@@ -25,18 +41,36 @@ function promisify (original) {
   }
 }
 
+/**
+ * Converts an object of node-style async methods
+ * into an interface who's methods return promises
+ * @param  {Object} target - the API to convert
+ * @return {Object} - the promisified interface
+ * @example
+ * let fs = promisifyAll(require('fs'))
+ * fs.readFile(file).then(...)
+ */
 function promisifyAll (target) {
   return Object.keys(target).reduce((promisified, key) => {
-    const value = target[key]
-    if (typeof value === 'function') {
-      promisified[key] = promisify(value)
-    } else {
-      promisified[key] = value
-    }
+    const method = target[key]
+    promisified[key] = typeof method === 'function'
+      ? promisify(target::method)
+      : method
     return promisified
   }, {})
 }
 
+/**
+ * asynchronous wrapper around fs.stat that
+ * detects if a file found at `path` exists or not
+ * @this   {Object} - promisified `fs` module
+ * @param  {String} path - the path to the file
+ * @return {Promise} - resolves to true if file exists
+ *                     else resolves to false
+ * @example
+ * let fs = promisifyAll(require('fs'))
+ * fs::exists(file).then(...)
+ */
 async function exists (path) {
   try {
     await this.stat(path)

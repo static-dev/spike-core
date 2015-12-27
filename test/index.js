a8d1fb7c86a2f818a65fc989e5533bed01658a9a
Jeff Escalante <jescalan@users.noreply.github.com>
2015-12-27 17:03:54 +0700

    validation for roots config looking good

--- test/index.js
+++ test/index.js
@@ -1,15 +1,18 @@
 import 'babel-core/register'
 import path from 'path'
 import fs from 'fs'
-// import rimraf from 'rimraf'
 import test from 'ava'
 import helpers from './_helpers'
+import Roots from '..'
+// import rimraf from 'rimraf'
+
+// const fixtures = path.join(__dirname, 'fixtures')
 
 // test.afterEach.cb((t) => {
 //   rimraf(t.context.publicPath, t.end)
 // })
 
-test('dump dirs', (t) => {
+test('dump dirs', t => {
   return helpers.compileFixture(t, 'dump_dirs').then((res) => {
     let indexPath = path.join(res.publicPath, 'index.html')
     t.ok(fs.existsSync(indexPath))
@@ -17,7 +20,7 @@ test('dump dirs', (t) => {
   })
 })
 
-test('ignores', (t) => {
+test('ignores', t => {
   return helpers.compileFixture(t, 'ignores').then((res, publicPath) => {
     let indexPath = path.join(res.publicPath, 'index.html')
     let aboutPath = path.join(res.publicPath, 'about.html')
@@ -29,14 +32,19 @@ test('ignores', (t) => {
   })
 })
 
-test('locals', (t) => {
+test('locals', t => {
   return helpers.compileFixture(t, 'locals').then((res, publicPath) => {
     let indexPath = path.join(res.publicPath, 'index.html')
     t.is(fs.readFileSync(indexPath, 'utf8'), 'bar')
   })
 })
 
-test('css', (t) => {
+test('config errors', t => {
+  t.throws((() => { new Roots() }), 'ValidationError: child "root" fails because ["root" is required]') // eslint-disable-line
+  t.throws( (() => { new Roots({ root: 'foo', matchers: 'wow' }) }), 'ValidationError: child "matchers" fails because ["matchers" must be an object]') // eslint-disable-line
+})
+
+test.skip('css', (t) => {
   return helpers.compileFixture(t, 'css').then((res, publicPath) => {
     console.log(res)
   })

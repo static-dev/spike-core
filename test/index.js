a7724aa9e42f12f6e67766feaa9fbc25f387d4bb
Jeff Escalante <hello@jenius.me>
2015-12-27 17:11:27 +0700

    more thorough tests for config validation

--- test/index.js
+++ test/index.js
@@ -40,8 +40,12 @@ test('locals', t => {
 })
 
 test('config errors', t => {
-  t.throws((() => { new Roots() }), 'ValidationError: child "root" fails because ["root" is required]') // eslint-disable-line
-  t.throws( (() => { new Roots({ root: 'foo', matchers: 'wow' }) }), 'ValidationError: child "matchers" fails because ["matchers" must be an object]') // eslint-disable-line
+  t.throws(() => { new Roots() }, 'ValidationError: child "root" fails because ["root" is required]') // eslint-disable-line
+  t.throws(() => { new Roots({ root: 'foo', matchers: 'wow' }) }, 'ValidationError: child "matchers" fails because ["matchers" must be an object]') // eslint-disable-line
+  t.throws(() => { new Roots({ root: 'foo', matchers: { css: [1] } }) }, 'ValidationError: child "matchers" fails because [child "css" fails because ["css" must be a string]]') // eslint-disable-line
+  t.throws(() => { new Roots({ root: 'foo', postCssPlugins: function () {} }) }, 'ValidationError: child "postCssPlugins" fails because ["postCssPlugins" must be an array]') // eslint-disable-line
+  t.throws(() => { new Roots({ root: 'foo', babelConfig: 'wow' }) }, 'ValidationError: child "babelConfig" fails because ["babelConfig" must be an object]') // eslint-disable-line
+  t.throws(() => { new Roots({ root: 'foo', bundleName: ['foo', 'bar'] }) }, 'ValidationError: child "bundleName" fails because ["bundleName" must be a string]') // eslint-disable-line
 })
 
 test.skip('css', (t) => {

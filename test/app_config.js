aca216781510783b5e9f07b6b038597b53d2ddd8
Jeff Escalante <hello@jenius.me>
2016-02-08 19:04:40 -0800

    tests

--- test/app_config.js
+++ test/app_config.js
@@ -5,10 +5,18 @@ import {
   path
 } from './_helpers'
 
-test('does not compile ignored files', (t) => {
+test('uses app.js configuration', (t) => {
   return compileFixture(t, 'app_config').then(({publicPath}) => {
     return fs.readFile(path.join(publicPath, 'index.html'), 'utf8')
   }).then((contents) => {
     return t.is(contents, 'override')
+  })
+})
+
+test('API config overrides app.js config', (t) => {
+  return compileFixture(t, 'app_config', { locals: { foo: 'double override' } }).then(({publicPath}) => {
+    return fs.readFile(path.join(publicPath, 'index.html'), 'utf8')
+  }).then((contents) => {
+    return t.is(contents, 'double override')
   })
 })

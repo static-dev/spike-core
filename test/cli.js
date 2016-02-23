7d4c7a5795f4e86f808e8a3f7d85a2c35dcabd3b
Jeff Escalante <hello@jenius.me>
2016-02-23 18:16:00 -0500

    cli test with injected mock

--- test/cli.js
+++ test/cli.js
@@ -1,7 +1,34 @@
-import { test } from './_helpers'
 import CLI from '../lib/cli'
+import {EventEmitter} from 'events'
+import { test } from './_helpers'
 
-test('compile', (t) => {
-  let cli = new CLI()
+// mock so that this only tests the CLI interface, not the roots tasks
+class RootsMock extends EventEmitter {
+  constructor (opts) {
+    super()
+    console.log('mock initialized')
+    this.opts = opts
+  }
+
+  compile () {
+    this.emit('compile', 'mock!')
+  }
+}
+
+test.before((t) => {
+  CLI.__Rewire__('Roots', RootsMock)
+})
+
+test.cb('compile', (t) => {
+  const cli = new CLI()
   t.ok(cli)
+
+  cli.on('error', t.end)
+  cli.on('warning', t.end)
+  cli.on('compile', (res) => {
+    t.is(res, 'mock!')
+    t.end()
+  })
+
+  cli.run('compile')
 })

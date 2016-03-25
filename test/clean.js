e198ad84f2aee23162c54f9d0b0208d76d5dfe3b
kylemac <kyle@carrotcreative.com>
2016-03-24 22:24:37 -0400

    implement roots.clean()

---
+++ test/clean.js
@@ -0,0 +1,28 @@
+import Roots from '..'
+import {
+  test,
+  compileFixture,
+  fixturesPath,
+  fs,
+  path
+} from './_helpers'
+
+test.before((t) => {
+  return compileFixture(t, 'clean')
+    .then(({publicPath}) => { return path.join(publicPath, 'index.html') })
+    .tap((index) => { return fs.stat(index).tap(t.ok.bind(t)) })
+    .then((index) => { return fs.readFile(index, 'utf8') })
+    .then((contents) => { return t.is(contents, '\n<p>override</p>') })
+})
+
+test.cb('emits clean message correctly', (t) => {
+  const project = new Roots({ root: path.join(fixturesPath, 'clean') })
+
+  project.on('error', t.end)
+  project.on('remove', (msg) => {
+    t.ok(msg.toString().match(/cleaned output directory/))
+    t.end()
+  })
+
+  project.clean()
+})

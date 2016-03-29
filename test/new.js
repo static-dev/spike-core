32001ff5a2b6a3328173b47cad4cd499191c8ee0
Jeff Escalante <jescalan@users.noreply.github.com>
2016-03-29 18:52:37 -0400

    API logic implemented, test passing

--- test/new.js
+++ test/new.js
@@ -1,14 +1,21 @@
 import test from 'ava'
 import Roots from '..'
 import path from 'path'
+import node from 'when/node'
+import rimraf from 'rimraf'
 import { fixturesPath } from './_helpers'
 
 test('creates a new roots project', (t) => {
-  return Roots.new({ root: path.join(fixturesPath, 'new_test'),
+  const testPath = path.join(fixturesPath, 'new_test')
+  return Roots.new({ root: testPath,
     locals: {
       name: 'test',
       description: 'test',
       github_username: 'test'
     }
+  }).then((instance) => {
+    t.is(instance.config.context, testPath)
+  }).finally(() => {
+    return node.call(rimraf, testPath)
   })
 })

b0603ec0cdf62d786693b42a9ba33cd0b5db6499
kylemac <kyle@carrotcreative.com>
2016-04-04 12:17:24 -0400

    add back a basic .css test

--- test/css.js
+++ test/css.js
@@ -5,8 +5,16 @@ import {
   path
 } from './_helpers'
 
-test('css plugin works', (t) => {
+test('css plugin works with .css', (t) => {
   return compileFixture(t, 'css')
+    .then(({publicPath}) => { return path.join(publicPath, 'main.css') })
+    .tap((base) => { return fs.stat(base).tap(t.ok.bind(t)) })
+    .then((base) => { return fs.readFile(base, 'utf8') })
+    .then((contents) => { return t.regex(contents, /color: pink/) })
+})
+
+test('css plugin works with .sss', (t) => {
+  return compileFixture(t, 'sugarss')
     .then(({publicPath}) => { return path.join(publicPath, 'main.css') })
     .tap((index) => { return fs.stat(index).tap(t.ok.bind(t)) })
     .then((index) => { return fs.readFile(index, 'utf8') })

ee97a78f240f0dd99d74e47acc727dee4ec8f082
Jeff Escalante <hello@jenius.me>
2016-04-06 08:32:53 -0400

    jade loader options to accept functions

--- test/locals.js
+++ test/locals.js
@@ -6,7 +6,7 @@ import {
 } from './_helpers'
 
 test('injects template locals', (t) => {
-  return compileFixture(t, 'locals', { locals: { foo: 'bar' } }).then(({publicPath}) => {
+  return compileFixture(t, 'locals', { locals: { foo: () => 'bar' } }).then(({publicPath}) => {
     return fs.readFile(path.join(publicPath, 'index.html'), 'utf8')
   }).then((contents) => {
     return t.is(contents, 'bar')

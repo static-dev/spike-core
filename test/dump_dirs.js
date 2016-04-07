a9952af72d156bb29f8180a803bc14fa4fcfa758
kylemac <kyle@carrotcreative.com>
2016-04-07 11:25:10 -0400

    update ava assertions for ava@0.14.0

--- test/dump_dirs.js
+++ test/dump_dirs.js
@@ -8,7 +8,7 @@ import {
 test('discards directories, but keeps the directory\'s files', (t) => {
   return compileFixture(t, 'dump_dirs')
     .then(({publicPath}) => { return path.join(publicPath, 'index.html') })
-    .tap((index) => { return fs.stat(index).tap(t.ok.bind(t)) })
+    .tap((index) => { return fs.stat(index).tap(t.truthy.bind(t)) })
     .then((index) => { return fs.readFile(index, 'utf8') })
     .then((contents) => { return t.is(contents, '\n<p>hello world!</p>') })
 })
--- test/plugins.js
+++ test/plugins.js
@@ -8,7 +8,7 @@ import {
 test('compiles a project with a custom plugin', (t) => {
   return compileFixture(t, 'plugins')
     .then(({publicPath}) => { return path.join(publicPath, 'index.html') })
-    .tap((index) => { return fs.stat(index).tap(t.ok.bind(t)) })
+    .tap((index) => { return fs.stat(index).tap(t.truthy.bind(t)) })
     .then((index) => { return fs.readFile(index, 'utf8') })
     .then((contents) => { return t.is(contents, 'doge') })
 })

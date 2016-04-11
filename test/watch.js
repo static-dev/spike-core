df87c3db0b321f020acf5ada577c77e708876a0a
Jeff Escalante <hello@jenius.me>
2016-04-11 10:26:57 -0400

    remove additional timeout

--- test/watch.js
+++ test/watch.js
@@ -19,10 +19,8 @@ test.cb('watches the project, reloads on modification', (t) => {
     i++
     if (i === 1) {
       const file = path.join(fixturesPath, 'watch/index.jade')
-      setTimeout(() => {
-        fs.appendFileSync(file, ' ')
-        fs.writeFileSync(file, fs.readFileSync(file, 'utf8').trim())
-      }, 100)
+      fs.appendFileSync(file, ' ')
+      fs.writeFileSync(file, fs.readFileSync(file, 'utf8').trim())
     }
     if (i === 2) {
       watcher.close()
@@ -31,5 +29,6 @@ test.cb('watches the project, reloads on modification', (t) => {
   })
 
   const watcher = project.watch()
+  // make sure the watcher is returned
   t.truthy((typeof watcher.startTime) === 'number')
 })

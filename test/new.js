dde534c22241329bb359546d8d2964c9b40f42b6
Jeff Escalante <hello@jenius.me>
2016-05-13 16:30:51 -0400

    fix new test

--- test/new.js
+++ test/new.js
@@ -3,13 +3,15 @@ const Spike = require('..')
 const path = require('path')
 const rimraf = require('rimraf')
 const Sprout = require('sprout')
+const mkdirp = require('mkdirp')
 const {EventEmitter} = require('events')
 const {fixturesPath} = require('./_helpers')
 
 test.cb('creates a new spike project', (t) => {
   // remove base template with sprout first
+  mkdirp.sync(Spike.tplPath)
   const sprout = new Sprout(Spike.tplPath)
-  sprout.remove('base').catch(() => {}).then(() => {
+  sprout.remove('base').catch(() => { }).then(() => {
     const testPath = path.join(fixturesPath, 'new_test')
     const emitter = new EventEmitter()
 

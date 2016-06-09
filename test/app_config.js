32ae4736ffa582c129ec8c4e8344efd6fa56c79b
Jeff Escalante <jescalan@users.noreply.github.com>
2016-06-09 16:13:46 -0400

    extra loader config for postcss and css loaders

--- test/app_config.js
+++ test/app_config.js
@@ -35,3 +35,16 @@ test('does not allow certain options to be configured', (t) => {
       t.truthy(res.stats.compilation.options.context !== 'override!')
     })
 })
+
+test('postcss and css querystring options', (t) => {
+  return compileFixture(t, 'app_config', {
+    postcss: { plugins: ['wow'], foo: 'bar' },
+    css: { foo: 'bar' }
+  }).then(({res}) => {
+    const opts = res.stats.compilation.options
+    t.truthy(opts.spike.postcssQuery.foo, 'bar')
+    t.truthy(opts.spike.css.foo, 'bar')
+    const cssLoaderConfig = opts.module.loaders.find((l) => l._core === 'css')
+    t.truthy(cssLoaderConfig.loader === 'css-loader?foo=bar!postcss-loader?foo=bar')
+  })
+})

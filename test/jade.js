340ee570e484ac9d6092492fdbde758de581a6cf
Jeff Escalante <hello@jenius.me>
2016-06-14 20:24:54 -0400

    starting to test posthtml with jade

--- test/jade.js
+++ test/jade.js
@@ -1,8 +1,11 @@
 const test = require('ava')
+const jadePlugin = require('posthtml-jade')
 const {compileFixture} = require('./_helpers')
 
 test('tracks jade front-end dependencies', (t) => {
-  return compileFixture(t, 'jade_dependencies').then(({res}) => {
+  return compileFixture(t, 'jade_dependencies', {
+    posthtml: { plugins: [jadePlugin({ pretty: true })] }
+  }).then(({res}) => {
     const fd = res.stats.compilation.fileDependencies.map((d) => {
       return d.replace(`${res.stats.compilation.options.context}/`, '')
     })

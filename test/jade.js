03acaba820edbb006aca3e76d93b67709d74b127
Jeff Escalante <jescalan@users.noreply.github.com>
2016-07-19 10:34:49 -0400

    upgrade posthtml loader (#164)

--- test/jade.js
+++ test/jade.js
@@ -1,7 +1,6 @@
 const test = require('ava')
 const path = require('path')
 const fs = require('fs')
-const url = require('url')
 const jadePlugin = require('posthtml-jade')
 const {compileFixture} = require('./_helpers')
 
@@ -9,8 +8,7 @@ test('works with jade plugin, tracks dependencies', (t) => {
   return compileFixture(t, 'jade', {
     matchers: { html: '**/*.jade' },
     posthtml: (ctx) => {
-      const path = url.parse(ctx.request.split('!').slice(-1)[0]).pathname
-      return { defaults: [jadePlugin({ filename: path, pretty: true })] }
+      return { plugins: [jadePlugin({ filename: ctx.requestPath, pretty: true })] }
     }
   }).then(({res, publicPath}) => {
     const index = fs.readFileSync(path.join(publicPath, 'index.html'), 'utf8')

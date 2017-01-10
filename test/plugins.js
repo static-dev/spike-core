8f509bb068a5fc2517dccf0e80793f38657c261c
Jeff Escalante <jescalan@users.noreply.github.com>
2017-01-10 11:52:09 -0500

    add spike processed file outPath option (#204)
    this allows plugins to change the output path of compiled files if
    they need to for any reason

--- test/plugins.js
+++ test/plugins.js
@@ -2,11 +2,13 @@ const test = require('ava')
 const path = require('path')
 const {compileFixture, fs} = require('./_helpers')
 
-test('compiles a project with a custom plugin', (t) => {
+test('compiles a project with a custom plugin, plugins can change output path', (t) => {
   return compileFixture(t, 'plugins')
     .then(({res, publicPath}) => {
       const index = path.join(publicPath, 'index.html')
+      const outputChanged = path.join(publicPath, 'changed_output.html')
       fs.statSync(index)
+      fs.statSync(outputChanged)
       t.truthy(res.stats.compilation.options.test === 'bar')
     })
 })

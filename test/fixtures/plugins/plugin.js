dcb6ef265a6c8fcc5fafe5f2b01f30d09ece8ac6
Jeff Escalante <escalantejeff@gmail.com>
2017-01-10 11:29:26 -0500

    add spike processed file outPath option
    this allows plugins to change the output path of compiled files if
    they need to for any reason

--- test/fixtures/plugins/plugin.js
+++ test/fixtures/plugins/plugin.js
@@ -4,11 +4,19 @@ module.exports = class JadeWebpackPlugin {
   }
 
   apply (compiler) {
-    compiler.plugin('run', function (compilation, done) {
-      setTimeout(function () {
+    compiler.plugin('run', (compilation, done) => {
+      setTimeout(() => {
         compiler.options.test = 'bar'
         done()
       }, 300)
+    })
+
+    compiler.plugin('emit', (compilation, done) => {
+      const file = compiler.options.spike.files.process.find((f) => {
+        if (f.path.match(/custom_output/)) { return f }
+      })
+      file.outPath = file.path.replace(/custom_output/, 'changed_output')
+      done()
     })
   }
 }

62011b109907092261e20ed7e88efabdb00f4d5d
Jeff Escalante <escalantejeff@gmail.com>
2017-02-08 16:21:42 -0500

    all tests passing

--- test/fixtures/static_plugins/plugin.js
+++ test/fixtures/static_plugins/plugin.js
@@ -1,11 +1,14 @@
+const SpikeUtil = require('spike-util')
+
 module.exports = class GladePlugin {
   constructor (opts) {
     this.opts = opts
   }
 
   apply (compiler) {
+    const util = new SpikeUtil(compiler.options)
     compiler.plugin('emit', function (compilation, done) {
-      const staticFiles = compiler.options.spike.files.static
+      const staticFiles = util.getSpikeOptions().files.static
       const gladeFiles = staticFiles.filter((f) => f.match(/\.glade$/))
       gladeFiles.forEach((f) => {
         const dep = compilation.modules.find((el) => {
